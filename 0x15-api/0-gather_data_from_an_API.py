86fa34c5aa7f5a2cd18d44eed44fe0541e70b7e1
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-10-23 13:10:33 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-'''
-script that, using this REST API, for a given employee ID, 
+'''script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 '''
-
 import requests
 import sys
 
